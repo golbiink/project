bf28c3a242f2bce2e91ada753ab6abdf6b6ecce8
golbiink <golbiink@gmail.com>
2017-06-13 04:54:58 +0700

    Add files via upload

---
+++ dina_meta2.php
@@ -0,0 +1,3 @@
+<?php
+		echo "jual minyak kemiri, minyak kemiri original, cream magic, cream bandung, cream biosof, jual cream kiloan, jual toner literan, cream racik bandung, cream magic quan, jual cream hn, jual crean syahrini, jual cream baby pink, cara merawat rambut, paket perawatan wajah, paket perawatan tubuh, cream magic bandun, sabun literan, jual cream qineta kiloan, jual cream mutiara bandung, jual cream walet gold super, walet gold super";
+?>
---
+++ media.php
@@ -0,0 +1,19 @@
+<?php 
+  ob_start();
+  session_start();	
+  include "config/koneksi.php";
+  include "config/fungsi_indotgl.php";
+  include "config/class_paging.php";
+  include "config/fungsi_combobox.php";
+  include "config/library.php";
+  include "config/fungsi_autolink.php";
+  include "config/fungsi_rupiah.php";
+  include "config/hapus_orderfiktif.php";
+
+  // Memilih template yang aktif saat ini
+  $pilih_template=mysql_query("SELECT folder FROM templates WHERE aktif='Y'");
+  $f=mysql_fetch_array($pilih_template);
+  include "$f[folder]/template.php"; 
+?>
+
+
